a8e6c2f5747fbc24707dfcffd221b13c1214a38f
Jon Martin <jonmartin@overstock.com>
2020-04-24 11:00:46 -0600

    Add challenge selection with color materials

---
+++ Assets/ChallengeMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class ChallengeMenu : MonoBehaviour {
+    public GameObject roundCornerCube;
+    public List<Material> challengeRowColors;
+    private List<GameObject> cubes = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start() {
+        for (var i = 17; i >= 0; i--) {
+            GameObject cube = Instantiate(roundCornerCube);
+            cube.transform.position = new Vector3(2 - (i % 3), i / 3, 0);
+            cube.GetComponent<Renderer>().material = challengeRowColors[i/3]; //GetMaterial();
+            cube.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.Off;
+            cube.transform.localScale = Vector3.one * .9f;
+
+            //cube.GetComponentInChildren<Text>().text = (18 - i).ToString();
+
+            cubes.Add(cube);
+        }
+    }
+}
---
+++ Assets/TileScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Vector3 mouseDownPosition;
+    void OnMouseDown() {
+        mouseDownPosition = Input.mousePosition;
+        Debug.Log(mouseDownPosition);
+        //screenPoint = Camera.main.WorldToScreenPoint(Input.mousePosition);
+        //offset = Input.mousePosition - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+    }
+  
+    void OnMouseDrag() {
+        Vector3 offset = Input.mousePosition - mouseDownPosition;
+        mouseDownPosition = Input.mousePosition;
+        if(offset.x < 0)
+            transform.position = transform.position.OffsetX(-offset.normalized.magnitude/100);
+        else {
+            transform.position = transform.position.OffsetX(offset.normalized.magnitude/100);
+        }
+       // Debug.Log(offset.normalized.magnitude);
+//        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z);
+//        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+//        transform.position = curPosition;
+    }
+}
+
+static class VectorExtensions {
+    public static Vector3 OffsetX(this Vector3 pos, float distance) {
+        return new Vector3(pos.x + distance, pos.y, pos.z);
+    }
+}
