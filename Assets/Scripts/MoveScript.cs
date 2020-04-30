c8687172edac3d9595784954905e52a097a63c00
Jon Martin <jonmartin@overstock.com>
2020-04-29 21:57:59 -0600

    Factored out tile creation from drag and wrap logic

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,33 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MoveScript : MonoBehaviour {
-    public GameObject roundCornerCube;
-    public List<Material> challengeRowColors;
     private List<GameObject> cubes = new List<GameObject>();
     public GameObject pointer;
 
     void Start() {
-        for (var i = 17; i >= 0; i--) {
-            GameObject cube = Instantiate(roundCornerCube);
-            cube.transform.position = new Vector3(2 - (i % 3), i / 3, 0);
-            cube.GetComponent<Renderer>().material = challengeRowColors[i / 3]; //GetMaterial();
-            cube.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.Off;
-            cube.transform.localScale = Vector3.one * .9f;
-
-            cube.GetComponentInChildren<Text>().text = (18 - i).ToString();
-            cube.name = (18 - i).ToString();
-            //cube.tag = "id here";
-            cubes.Add(cube);
-        }
+        cubes = GetComponent<TileField>().GetTiles();
     }
-
+    
     // Update is called once per frame
     void Update() {
+        
         if (Input.GetKeyDown(KeyCode.Escape))
             SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
---
+++ Assets/Scripts/TileField.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public interface TileField {
+    List<GameObject> GetTiles();
+}
