a1a5c2247a4006f7bb64deaa3a8514c7abadb3c0
Jon Martin <jonmartin@overstock.com>
2020-05-02 08:06:44 -0600

    Initial refactor started

--- Assets/Scripts/InputTracker.cs
+++ Assets/Scripts/InputTracker.cs
@@ -1,5 +1,100 @@
+using System;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
 namespace DefaultNamespace {
     public class InputTracker {
+        private Vector3 mouseDownPosition;
+        private Vector3 lockedPosition;
+        private Vector3 screenPoint;
+        private Vector3 offset;
+        private Vector3 initialWorldPosition;
+        private int itemIndex;
+        private bool inDrag;
+        private Vector3 dragAxis;
+        Vector3 dragDirection = Vector3.zero;
+        Vector3 curPosition = Vector3.zero;
         
+        public void StartTracking(Vector3 initialMouseDownPosition, Vector3 initialTransformPosition) {
+            initialWorldPosition = initialTransformPosition;
+            dragAxis = Vector3.zero;
+            dragDirection = Vector3.zero;
+            
+            mouseDownPosition = initialMouseDownPosition;
+            screenPoint = Camera.main.WorldToScreenPoint(initialTransformPosition);
+
+            offset = initialTransformPosition -
+                     Camera.main.ScreenToWorldPoint(
+                         new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+            curPosition = offset;
+            inDrag = true;
+        }
+
+        public void Update(Action<Vector3, float> updateFunc) {
+            if (!inDrag || dragDirection == Vector3.zero)
+                return;
+            
+            updateFunc.Invoke(dragDirection, (initialWorldPosition - (curPosition + offset)).magnitude);
+            initialWorldPosition = (curPosition + offset);
+        }
+
+        private Action endFunc;
+        public void OnEnd(Action func) {
+            endFunc = func;
+        }
+
+        public void End() {
+            if (!inDrag)
+                return;
+            endFunc();
+        }
+        public void UpdateTracking(Vector3 mousePosition) {
+            if (!inDrag)
+                return;
+            
+            var diff = mouseDownPosition - mousePosition;
+            if (!(diff.magnitude > 5f)) return;
+
+            if (dragAxis == Vector3.zero) {
+                if (Math.Abs(diff.x) > Math.Abs(diff.y)) {
+                    lockedPosition = new Vector3(0, mouseDownPosition.y, 0);
+                    dragAxis = Vector3.right;
+                }
+                else {
+                    lockedPosition = new Vector3(mouseDownPosition.x, 0, 0);
+                    dragAxis = Vector3.up;
+                }
+            }
+
+            var curScreenPoint = new Vector3(Input.mousePosition.x * dragAxis.x + lockedPosition.x,
+                Input.mousePosition.y * dragAxis.y + lockedPosition.y,
+                screenPoint.z);
+
+            curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            
+            // if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).x) >= 1.1 ||
+            //     Mathf.Abs((initialWorldPosition - (curPosition + offset)).y) >= 1.1) {
+            //     endFunc();
+            //     inDrag = false;
+            //     return;
+            // }
+            
+            if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).x) >= 1.0) {
+                if ((initialWorldPosition - (curPosition + offset)).x > 0)
+                    dragDirection = Vector3.left;
+                else {
+                    dragDirection = Vector3.right;
+                }
+            }
+            else if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).y) >= 1.0) {
+                if ((initialWorldPosition - (curPosition + offset)).y < 0)
+                    dragDirection = Vector3.up;
+                else {
+                    dragDirection = Vector3.down;
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,51 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Vector3 = UnityEngine.Vector3;
 
 public class MoveScript : MonoBehaviour {
     private List<GameObject> cubes = new List<GameObject>();
     public GameObject pointer;
-
+    private Vector3 savedPosition;
     void Start() {
         cubes = GetComponent<TileField>().GetTiles();
+        savedPosition = transform.position;
+        inputTracker.OnEnd(() => {
+            //transform.position = incrementalTransformPosition;
+
+            foreach (var cube in cubes) {
+                cube.transform.parent = null;
+                var position = cube.transform.position;
+                cube.transform.position = new Vector3(Mathf.Round(position.x), Mathf.Round(position.y), position.z);
+            }
+
+            movingCubesCollection.DestroyWrapClones();
+            movingCubesCollection = null;
+
+            transform.position = savedPosition;
+        });
     }
-    
+
+    private float accumulatedMag;
     // Update is called once per frame
     void Update() {
         
         if (Input.GetKeyDown(KeyCode.Escape))
             SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        
+        inputTracker.Update((direction, magnitude) => {
+            accumulatedMag += magnitude;
+            if (movingCubesCollection == null) {
+                if (direction == Vector3.left || direction == Vector3.right)
+                    movingCubesCollection = TileMover.GetMovingCubesCollection(dragChild, cubes, TileMover.GetRow);
+                else
+                    movingCubesCollection = TileMover.GetMovingCubesCollection(dragChild, cubes, TileMover.GetColumn);
+
+                movingCubesCollection.AttachToMoverParent(transform);
+                //movingCubesCollection.ClampPositions();
+            }
+
+            if (accumulatedMag > 1.0) {
+                if (direction == Vector3.left) {
+                    movingCubesCollection.LeftWrap();
+                    movingCubesCollection.ProvideWrapClones(transform);
+                }
+                else if (direction == Vector3.right) {
+                    movingCubesCollection.RightWrap();
+                    movingCubesCollection.ProvideWrapClones(transform);
+                }
+                else if (direction == Vector3.up) {
+                    movingCubesCollection.UpWrap();
+                    movingCubesCollection.ProvideWrapClones(transform);
+                }
+                else {
+                    movingCubesCollection.DownWrap();
+                    movingCubesCollection.ProvideWrapClones(transform);
+                }
+            }
+
+            transform.position += direction * magnitude;
+        });
     }
 
-    private Vector3 mouseDownPosition;
-    private Vector3 lockedPosition;
-    private Vector3 screenPoint;
-    private Vector3 offset;
-    private Vector3 initialWorldPosition;
-    private GameObject dragChild;
-    private int itemIndex;
-    private bool inDrag;
-    private Vector3 savedTransformPosition;
-    private Vector3 incrementalTransformPosition;
-
     private MovingCubesCollection movingCubesCollection;
-
+    private InputTracker inputTracker = new InputTracker();
+    private GameObject dragChild;
     RaycastHit[] raycastHits = new RaycastHit[5];
-
+    
     void OnMouseDown() {
-        var initialTransformPosition = transform.position;
-        savedTransformPosition = initialTransformPosition;
-        incrementalTransformPosition = initialTransformPosition;
-        initialWorldPosition = initialTransformPosition;
-
-        mouseDownPosition = Input.mousePosition;
-        screenPoint = Camera.main.WorldToScreenPoint(initialTransformPosition);
-
-        offset = initialTransformPosition -
-                 Camera.main.ScreenToWorldPoint(
-                     new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         Physics.RaycastNonAlloc(ray, raycastHits);
@@ -58,105 +89,89 @@ public class MoveScript : MonoBehaviour {
 
         if (dragChild == null)
             return;
-
-        inDrag = true;
+        
+        inputTracker.StartTracking(Input.mousePosition, transform.position);
     }
 
     private void OnMouseDrag() {
-        if (!inDrag)
-            return;
+        inputTracker.UpdateTracking(Input.mousePosition);
 
-        if (movingCubesCollection == null) {
-            var diff = Input.mousePosition - mouseDownPosition;
-            if (!(diff.magnitude > 5f)) return;
+        // if (movingCubesCollection == null) {
+        //     var diff = Input.mousePosition - mouseDownPosition;
+        //     if (!(diff.magnitude > 5f)) return;
+        //
+        //     if (Math.Abs(diff.x) > Math.Abs(diff.y)) {
+        //         lockedPosition = new Vector3(0, mouseDownPosition.y, 0);
+        //         movingCubesCollection = TileMover.GetMovingCubesCollection(dragChild, cubes, TileMover.GetRow);
+        //     }
+        //     else {
+        //         lockedPosition = new Vector3(mouseDownPosition.x, 0, 0);
+        //         movingCubesCollection = TileMover.GetMovingCubesCollection(dragChild, cubes, TileMover.GetColumn);
+        //     }
+        //
+        //     movingCubesCollection.AttachToMoverParent(transform);
+        //     movingCubesCollection.ProvideWrapClones(transform);
+        // }
+        //
+        // var curScreenPoint = new Vector3(Input.mousePosition.x * movingCubesCollection.dragAxis.x + lockedPosition.x,
+        //     Input.mousePosition.y * movingCubesCollection.dragAxis.y + lockedPosition.y,
+        //     screenPoint.z);
+        //
+        // Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+        // Vector3 dragDirection = Vector3.zero;
+        //
+        // if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).x) >= 1.1 ||
+        //     Mathf.Abs((initialWorldPosition - (curPosition + offset)).y) >= 1.1) {
+        //     OnMouseUp();
+        //     inDrag = false;
+        //     return;
+        // }
+        //
+        // if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).x) >= 1.0) {
+        //     if ((initialWorldPosition - (curPosition + offset)).x > 0)
+        //         dragDirection = Vector3.left;
+        //     else {
+        //         dragDirection = Vector3.right;
+        //     }
+        // }
+        // else if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).y) >= 1.0) {
+        //     if ((initialWorldPosition - (curPosition + offset)).y < 0)
+        //         dragDirection = Vector3.up;
+        //     else {
+        //         dragDirection = Vector3.down;
+        //     }
+        // }
+        //
+        // if (dragDirection != Vector3.zero) {
+        //     incrementalTransformPosition += dragDirection;
+        //     initialWorldPosition = curPosition + offset;
+        //     pointer.transform.position = offset;
+        //     
+        //     movingCubesCollection.DestroyWrapClones();
+        //     movingCubesCollection.ClampPositions();
+        //     
+        //     if (dragDirection == Vector3.left) {
+        //         movingCubesCollection.LeftWrap();
+        //         movingCubesCollection.ProvideWrapClones(transform);
+        //     }
+        //     else if (dragDirection == Vector3.right) {
+        //         movingCubesCollection.RightWrap();
+        //         movingCubesCollection.ProvideWrapClones(transform);
+        //     }
+        //     else if (dragDirection == Vector3.up) {
+        //         movingCubesCollection.UpWrap();
+        //         movingCubesCollection.ProvideWrapClones(transform);
+        //     }
+        //     else {
+        //         movingCubesCollection.DownWrap();
+        //         movingCubesCollection.ProvideWrapClones(transform);
+        //     }
+        // }
 
-            if (Math.Abs(diff.x) > Math.Abs(diff.y)) {
-                lockedPosition = new Vector3(0, mouseDownPosition.y, 0);
-                movingCubesCollection = TileMover.GetMovingCubesCollection(dragChild, cubes, TileMover.GetRow);
-            }
-            else {
-                lockedPosition = new Vector3(mouseDownPosition.x, 0, 0);
-                movingCubesCollection = TileMover.GetMovingCubesCollection(dragChild, cubes, TileMover.GetColumn);
-            }
-
-            movingCubesCollection.AttachToMoverParent(transform);
-            movingCubesCollection.ProvideWrapClones(transform);
-        }
-
-        var curScreenPoint = new Vector3(Input.mousePosition.x * movingCubesCollection.dragAxis.x + lockedPosition.x,
-            Input.mousePosition.y * movingCubesCollection.dragAxis.y + lockedPosition.y,
-            screenPoint.z);
-
-        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-        Vector3 dragDirection = Vector3.zero;
-
-        if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).x) >= 1.1 ||
-            Mathf.Abs((initialWorldPosition - (curPosition + offset)).y) >= 1.1) {
-            OnMouseUp();
-            inDrag = false;
-            return;
-        }
-
-        if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).x) >= 1.0) {
-            if ((initialWorldPosition - (curPosition + offset)).x > 0)
-                dragDirection = Vector3.left;
-            else {
-                dragDirection = Vector3.right;
-            }
-        }
-        else if (Mathf.Abs((initialWorldPosition - (curPosition + offset)).y) >= 1.0) {
-            if ((initialWorldPosition - (curPosition + offset)).y < 0)
-                dragDirection = Vector3.up;
-            else {
-                dragDirection = Vector3.down;
-            }
-        }
-
-        if (dragDirection != Vector3.zero) {
-            incrementalTransformPosition += dragDirection;
-            initialWorldPosition = curPosition + offset;
-            pointer.transform.position = offset;
-            
-            movingCubesCollection.DestroyWrapClones();
-            movingCubesCollection.ClampPositions();
-            
-            if (dragDirection == Vector3.left) {
-                movingCubesCollection.LeftWrap();
-                movingCubesCollection.ProvideWrapClones(transform);
-            }
-            else if (dragDirection == Vector3.right) {
-                movingCubesCollection.RightWrap();
-                movingCubesCollection.ProvideWrapClones(transform);
-            }
-            else if (dragDirection == Vector3.up) {
-                movingCubesCollection.UpWrap();
-                movingCubesCollection.ProvideWrapClones(transform);
-            }
-            else {
-                movingCubesCollection.DownWrap();
-                movingCubesCollection.ProvideWrapClones(transform);
-            }
-        }
-
-        transform.position = curPosition + offset;
+        //transform.position = curPosition + offset;
     }
 
     void OnMouseUp() {
-        if (!inDrag)
-            return;
-
-        transform.position = incrementalTransformPosition;
-
-        foreach (var cube in cubes) {
-            cube.transform.parent = null;
-            var position = cube.transform.position;
-            cube.transform.position = new Vector3(Mathf.Round(position.x), Mathf.Round(position.y), position.z);
-        }
-
-        movingCubesCollection.DestroyWrapClones();
-        movingCubesCollection = null;
-
-        transform.position = savedTransformPosition;
-        inDrag = false;
+        inputTracker.End();
     }
 }
